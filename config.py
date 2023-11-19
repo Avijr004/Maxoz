1a09af7550349535d7f9ff89757d1b3290c7c4ec
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-11-19 18:54:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://telegra.ph/file/d37e12b738025daa428ea.mp4",
+    "https://github.com/Avijr004/Maxoz",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
